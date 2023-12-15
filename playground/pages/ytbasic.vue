79cbde7b6f71bff19642d464782347106d572b57
Thorsten Kober <tkober@gmail.com>
2023-12-15 17:41:06 -0500

    add basic iframe

---
+++ playground/pages/ytbasic.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <iframe id="ytplayer" type="text/html" width="640" height="360"
+  src="https://www.youtube.com/embed/rT1MTP-Rrt8?autoplay=1&origin=http://example.com"
+  frameborder="0"></iframe>
+    </div>
+</template>
