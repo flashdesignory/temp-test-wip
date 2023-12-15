a19745f1483edfdc88884985aed3ba03adf671c4
Thorsten Kober <tkober@gmail.com>
2023-12-15 18:15:26 -0500

    maps basic

--- playground/pages/ytbasic.vue
+++ playground/pages/ytbasic.vue
@@ -1,7 +1,12 @@
 <template>
-    <div>
-        <iframe id="ytplayer" type="text/html" width="640" height="360"
-  src="https://www.youtube.com/embed/rT1MTP-Rrt8?autoplay=1&origin=http://example.com"
-  frameborder="0"></iframe>
-    </div>
+  <div>
+    <iframe
+      id="ytplayer"
+      type="text/html"
+      width="640"
+      height="360"
+      src="https://www.youtube.com/embed/rT1MTP-Rrt8?autoplay=1&origin=http://example.com"
+      frameborder="0"
+    />
+  </div>
 </template>
