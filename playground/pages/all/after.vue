0d4232fc9f99d89406080646ac4565bb120e61a8
Thorsten Kober <thorstenk@google.com>
2024-03-04 21:48:16 -0500

    all

---
+++ playground/pages/all/after.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { useGoogleAnalytics, useGoogleTagManager } from '#imports'
+
+useGoogleAnalytics({
+  id: 'GA-123456789-1',
+})
+
+useGoogleTagManager({
+  id: 'GTM-MNJD4B',
+})
+</script>
+
+<template>
+  <div>
+    <div>
+      <GoogleMapsJavaScriptApi
+        api-key="AIzaSyCFj8Vni52FfC9L1zpzxqH3ViUPmioa-Dw"
+        width="600"
+        height="400"
+        :center="{ lat: 40.689247, lng: -74.044502 }"
+      />
+    </div>
+    <div>
+      <YoutubeEmbed
+        videoid="rT1MTP-Rrt8"
+        playlabel="Play"
+      />
+    </div>
+  </div>
+</template>
---
+++ playground/pages/all/before.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { useHead } from '#imports'
+import googleMapsCommunity from '../maps/google-maps-community.vue';
+const gtagID = 'G-MMQSNF1HQ5';
+useHead({
+    script: [
+        {
+            key: 'gtag-setup',
+            innerHTML: `window.dataLayer=window.dataLayer||[];window.gtag=function gtag() {window.dataLayer.push(arguments)};window.gtag('js',new Date());window.gtag('config', ${JSON.stringify(gtagID)})`,
+        },
+        {
+            src: `https://www.googletagmanager.com/gtag/js?id=${gtagID}`,
+            onload: (event) => "loaded!" && console.log(event),
+        },
+    ]
+});
+
+const gtmID = 'G-MMQSNF1HQ5';
+useHead({
+    script: [
+        {
+            key: 'gtm-setup',
+            innerHTML: 'window.dataLayer=window.dataLayer||[];window.dataLayer.push({\'gtm.start\':new Date().getTime(),event:\'gtm.js\'});',
+        },
+        {
+            src: `https://www.googletagmanager.com/gtm.js?id=${gtmID}`,
+            onload: (event) => "loaded!" && console.log(event),
+        },
+    ]
+});
+</script>
+
+<template>
+  <div>
+    <div>
+      <googleMapsCommunity />
+    </div>
+    <div>
+      <div>
+        <iframe
+          id="ytplayer"
+          type="text/html"
+          width="640"
+          height="360"
+          src="https://www.youtube.com/embed/rT1MTP-Rrt8"
+          frameborder="0"
+        />
+      </div>
+    </div>
+  </div>
+</template>
